f45da7a88c381489fb29c100b5905c94eaffba20
Igor Trevelin <igor.trevelin.xavier@gmail.com>
2022-07-14 20:37:36 -0300

    added development freepas on admin guard

--- src/modules/auth/admin.guard.ts
+++ src/modules/auth/admin.guard.ts
@@ -1,25 +1,29 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { ADMIN_ONLY } from './decorators';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly configService: ConfigService,
+  ) {}
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const admin = this.reflector.get<boolean>(ADMIN_ONLY, context.getHandler());
 
-    if (admin) {
-      const req = context.switchToHttp().getRequest();
-      const user = req.user;
-      if (user.admin) return true;
+    const env = this.configService.get('NODE_ENV');
 
-      return false;
-    }
+    if (!admin || env === 'development') return true;
 
-    return true;
+    const req = context.switchToHttp().getRequest();
+    const user = req.user;
+    if (user.admin) return true;
+
+    return false;
   }
 }
